1c9d93a91bf7c4ebe03ab3423010bbea1f237c74
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-09-20 11:29:15 +0300

    configs for prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import {SpeedInsights} from '@vercel/speed-insights/next';
 import {Analytics} from '@vercel/analytics/next';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
+
 import './globals.css';
 
 const geistSans = Geist({
