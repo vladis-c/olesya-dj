f6975e383bff595c776caff4b447fa0c2507b5ce
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-09-20 11:22:19 +0300

    added vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import {SpeedInsights} from '@vercel/speed-insights/next';
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
@@ -27,6 +28,7 @@ const RootLayout = ({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
