e9db300f55d79c4c21a2ade39720f920041e419c
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-09-21 12:44:39 +0300

    removed unnecessary import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import {Analytics} from '@vercel/analytics/next';
 import {SpeedInsights} from '@vercel/speed-insights/next';
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
-import Link from 'next/link';
 import Button from '@/lib/components/Button';
 import './globals.css';
 
