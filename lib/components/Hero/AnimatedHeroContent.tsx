ddb9be417d14be894c4707b9b27f05a5daf5dbed
Vladislav Cherkasheninov <vladcher26@yandex.ru>
2025-10-10 14:43:41 +0300

    added fade out of Hero content

---
+++ lib/components/Hero/AnimatedHeroContent.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import {motion, useScroll, useTransform} from 'framer-motion';
+import {useEffect, useState} from 'react';
+
+type AnimatedHeroContentProps = {
+  children: React.ReactNode;
+};
+
+const AnimatedHeroContent = ({children}: AnimatedHeroContentProps) => {
+  const [heroHeight, setHeroHeight] = useState(0);
+  const {scrollY} = useScroll();
+
+  useEffect(() => {
+    const heroElement = document.getElementById('hero');
+    if (heroElement) {
+      setHeroHeight(heroElement.offsetHeight);
+    }
+  }, []);
+
+  const opacity = useTransform(scrollY, [0, heroHeight * 0.7], [1, 0]);
+  const y = useTransform(scrollY, [0, heroHeight * 0.6], [0, -100]);
+
+  return <motion.div style={{opacity, y}}>{children}</motion.div>;
+};
+
+export default AnimatedHeroContent;
